a7baae08d331cd925672e3f6e6ba7b33b9cba200
Esosa Oviasu <koopaoviasu@gmail.com>
2020-06-28 15:31:52 -0400

    clean code

--- index.js
+++ index.js
@@ -1,44 +1,76 @@
 const apikey = 'B5apR3q4ufhJN72IrcWKLD3LBf0DJ3HcxzWbPwgV'
+const searchUrl = 'https://developer.nps.gov/api/v1/parks'
 
-function displayParkList(responseJson) {
-    console.log(responseJson.length);
-    for (i = 0; i < responseJson.length; i++) {
-        $('.results').append(`<li class="result-list-item">
-        <h2>${responseJson[i].name}</h2>
-            <a href="${responseJson[i].html_url}" class="resultsRepos">${responseJson[i].html_url}</a>
-    </li>`);
+
+
+function displayParkList(responseJson, maxResults) {
+    console.log(responseJson.data[1].description)
+    for (i = 0; i < responseJson.data.length & i < maxResults; i++) {
+        $('.results').append(`
+        <li class="result-list-item">
+            <div>
+                <h3 class="parkName">${responseJson.data[i].name}</h3>
+            </div>
+            <div>
+                <h4 class="parkAddress">${responseJson.data[i].addresses[0].line1},${responseJson.data[i].addresses[0].city}, ${responseJson.data[i].addresses[0].stateCode}</h4>
+            </div>
+            <div>
+                <p class="parkDescription">${responseJson.data[i].description}</p>
+            </div>
+            <div>
+                <a href="${responseJson.data[i].url}" class="parkUrl">${responseJson.data[i].url}</a>
+            </div>
+        </li>
+    `);
     };
-    $('ol').removeClass('hidden');
-
+    $('ul').removeClass('hidden');
+    console.log(responseJson.data.length);
     
 };
 
 
+function reformatQueries(param) {
+    const queryItems = Object.keys(param).map(key => `${key}=${param[key]}`);
+    return queryItems.join('&')
+};
 
-function getParkList() {
-    const searchUrl = `https://api.github.com/users/${$('input').val()}/repos`
-    fetch(searchUrl)
+
+function getParkList(seaTer, maxRes=10) {
+    const param = {
+        stateCode: seaTer,
+        limit: maxRes,
+        api_key: apikey,
+    };
+
+    const queryString = reformatQueries(param);
+    const url = searchUrl+'?'+queryString;
+
+    fetch(url)
     .then(response => {
         if (response.ok) {
-            return response.json()
+            return response.json();
         }
         throw new Error(response.statusText);
     })
-    .then(responseJson => displayParkList(responseJson))
-    .catch(error => alert('User not found'));
+    .then(responseJson => displayParkList(responseJson, maxRes))
+    .catch(error => alert(`There seems to be some technical difficulties: ${error.message}`));
 };
 
 
 
 function wipeResults() {
-    $('ol').empty();
+    $('ul').empty();
 };
 
-$(function runParkList() {
+function runParkList() {
     $('button').click( function(event) {
         event.preventDefault();
-        console.log($('input').val());
+        const searchTerm = $('#state').val();
+        const maxResults = $('#max-num').val();
+        console.log(searchTerm);
         wipeResults();
-        getParkList();
+        getParkList(searchTerm, maxResults);
     });
-});
+};
+
+runParkList();
